add56fd357e4dc3703ee6ff50a1fc52dd23dc7fb
alpacayejin <89776321+alpacayejin@users.noreply.github.com>
2025-05-28 21:04:06 +0900

    Update and rename sql_injection to sql_injection_scanner.py

--- sql_injection_scanner.py
+++ sql_injection_scanner.py
@@ -1,5 +1,5 @@
-# modules/sql_injection.py
 import requests
+
 from urllib.parse import urlparse, parse_qs, urlencode
 
 SQLI_PAYLOADS = ["'", "' OR '1'='1", "' OR 1=1--", "';--", "\" OR \"1\"=\"1"]
@@ -34,3 +34,32 @@ def test_sqli(url):
 def log_result(vuln_type, url):
     with open("report/result.txt", "a", encoding="utf-8") as f:
         f.write(f"[{vuln_type}] {url}\n")
+
+
+import requests
+from .payloads import sql_payloads
+from .form_extractor import get_forms, get_form_details
+
+def is_sql_error(response_text):
+    errors = ["you have an error in your sql syntax", "warning: mysql", "unclosed quotation", "sqlite error"]
+    return any(error in response_text.lower() for error in errors)
+
+def scan_sql_injection(url):
+    forms = get_forms(url)
+    vulnerable = []
+    for form in forms:
+        form_details = get_form_details(form, url)
+        for payload in sql_payloads:
+            data = {}
+            for input in form_details['inputs']:
+                if input['type'] != 'submit' and input['name']:
+                    data[input['name']] = payload
+            if form_details['method'] == 'post':
+                res = requests.post(form_details['action'], data=data)
+            else:
+                res = requests.get(form_details['action'], params=data)
+            if is_sql_error(res.text):
+                vulnerable.append((form_details, payload))
+                break
+    return vulnerable
+
